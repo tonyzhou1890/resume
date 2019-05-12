77b25869e3188edc4f6452b02949902c6ee43090
Tony <1332988826@qq.com>
2019-05-12 16:15:31 +0800

    pdf done

--- src/views/main/components/body.vue
+++ src/views/main/components/body.vue
@@ -146,39 +146,24 @@
         <li
           v-for="(item,index) in work_list"
           :key="index + 1"
-          v-show="item.filter"
         >
-          <router-link
-            v-if="item.path"
-            :to="item.path"
-          >
-            <img
-              :src="item.pic"
-            >
-            <div class="work-list-title">
-              <p
-                v-text="item.title"
-              ></p>
-            </div>
-          </router-link>
-          <a
-            v-else
-            :href="item.href"
-            target="_blank"
-          >
-            <img
-              :src="item.pic"
-            >
-            <div class="work-list-title">
-              <p
-                v-text="item.title"
-              ></p>
-            </div>
-          </a>
+          <header class="header">
+            <h3 class="title">{{ item.title }}</h3>
+            <span class="time">{{ item.time }}</span>
+          </header>
+          <hr />
+          <p class="belong">{{ item.belong }}</p>
+          <div class="content">
+            <p class="item-name">项目描述：</p>
+            <p class="item-content" v-html="item.description"></p>
+          </div>
+          <div class="content">
+            <p class="item-name">责任描述：</p>
+            <p class="item-content" v-html="item.duty"></p>
+          </div>
         </li>
       </transition-group>
     </section>
-    <p class="copyright">© 2019 Tony</p>
   </div>
 </template>
 
@@ -299,40 +284,46 @@ export default {
     work_list: function() {
       return [
         {
-          pic: '/static/images/xiaowei-company/cover.jpg',
-          filter: this.work.company,
-          path: '/portfolio/xiaowei-company',
-          title: '小为官网'
+          title: '小为官网',
+          time: '2019.03-2019.03',
+          belong: '小为智能科技有限公司',
+          description: `小为官方网站<br /><br />技术栈：vue、vue-router、vue-meta、axios、element-ui、百度地图、预渲染。<br /><br />官网由首页、小为产品、解决方案、商业合作、关于小为、联系我们几个部分组成。路由模式采用history，seo方案为预渲染。<br /><br />地址：www.xiaowei-tech.com`,
+          duty: `负责项目框架的搭建、页面的编写、服务端nginx配置、预渲染。<br /><br />页面负责小为产品、商务合作、联系我们三个版块。`
         },
         {
-          pic: '/static/images/xiaowei-data-display-v4/cover.jpg',
-          filter: this.work.company,
-          path: '/portfolio/xiaowei-data-display',
-          title: '小为数据展示平台'
+          title: '小为数据展示平台',
+          time: '2019.02-2019.03',
+          belong: '小为智能科技有限公司',
+          description: `技术栈：vue、vuex、vue-router、axios、element-ui、vue-element-admin、echarts。<br /><br />数据可视化项目，为了展示设备相关数据。项目结构为：总首页->场景首页->场景详情页。<br /><br />整个项目基本都是联动的。地图根据不同的场景可以下钻不同的层级，同时周边的图表根据地图层级和地点变化。首页和详情页的数据都通过轮询获取。大部分数据都使用 vuex 管理。`,
+          duty: `负责框架的搭建、整体布局和部分页面的编写。<br /><br />页面包括头部导航栏、各场景地图、地图周边数据对接、详情页设备列表、历史数据、水质和井盖最新数据、设备信息、场景信息、数据分析等。`
         },
         {
-          pic: '/static/images/xiaowei-backend-v2/cover.jpg',
-          filter: this.work.company,
-          path: '/portfolio/xiaowei-backend',
-          title: '小为后台管理系统'
+          title: '设备管理系统',
+          time: '2019.01-2019.02',
+          belong: '小为智能科技有限公司',
+          description: `技术栈：vue、vuex、vue-router、axios、element-ui、vue-element-admin。<br /><br />这个设备管理系统（或者说后台管理系统都可以）是为了管理第三方公司和设备的。`,
+          duty: `负责框架的搭建、权限模块和部分页面的编写。<br /><br />页面包括菜单管理、组织管理、账号管理、角色管理、地区管理。`
         },
         {
-          pic: '/static/images/poem-v3/cover.jpg',
-          filter: this.work.self,
-          path: '/portfolio/poem',
-          title: '诗词'
+          title: '个人简历',
+          time: '2018.07-至今',
+          belong: '个人',
+          description: `技术栈：vue + vue-router<br /><br />地址： http://resume.tony93.top`,
+          duty: `设计+前端开发`
         },
         {
-          pic: '/static/images/tiny-utils.js/cover.png',
-          filter: this.work.self,
-          href: 'https://github.com/tonyzhou1890/tiny-utils.js',
-          title: 'tiny-utils.js'
+          title: '诗词网站',
+          time: '2018.06-至今',
+          belong: '个人',
+          description: `一个可以查看大量古典诗词（包括部分近现代）的网站。可以查看随机诗词，可以搜索标题、诗人、内容，可以查看诗人列表。<br /><br />网站兼容移动端和 ie9+。`,
+          duty: `网站设计+前端开发+后端开发。`
         },
         {
-          pic: '/static/images/more/cover.png',
-          filter: true,
-          href: 'http://tony93.top/?page_id=204',
-          title: '更多……'
+          title: 'tiny-utils.js',
+          time: '2019.01-至今',
+          belong: '个人',
+          description: `个人用代码库。<br /><br />地址：https://github.com/tonyzhou1890/tiny-utils.js`,
+          duty: `base 部分——类型判定<br />string 部分——字符串方法<br />array 部分——数组方法<br />object 部分——对象方法<br >functions 部分——函数工具<br />date 部分——日期方法<br />validate 部分——验证方法<br />tree 部分——树方法<br />UI 部分——第三方库ui增强或自实现ui组件<br />directive 部分——vue指令`
         }
       ]
     }
@@ -353,6 +344,9 @@ $white: white;
   padding: 1.5rem;
   color: #444;
   font-family: "Junction",Helvetica,Arial,sans-serif;
+  ul {
+    margin-top: 0;
+  }
   .title {
     height: 4.7rem;
     line-height: 4.7rem;
@@ -466,46 +460,42 @@ $white: white;
     .work-list {
       overflow: hidden;
       li {
-        width: 33%;
-        float: left;
         overflow: hidden;
         position: relative;
         padding: 0.2rem;
         border-radius: 0.4rem;
         box-sizing: border-box;
-        img {
-          width: 100%;
-          height: 100%;
-          vertical-align: middle;
-        }
-        div {
-          opacity: 0;
-          background: $white;
-          position: absolute;
-          top: 0;
-          left: 0;
-          width: 100%;
-          height: 100%;
-          text-align: center;
-          padding: 0;
-          margin: 0;
-          color: #333;
-          font-size: 20px;
-          font-weight: bold;
-          transition: opacity .5s;
-          p {
-            position: absolute;
-            top: 0;
-            bottom: 0;
-            left: 0;
-            right: 0;
-            margin: auto;
-            font-size: 1.5rem;
-            height: 2rem;
+        .header {
+          .title {
+            display: inline-block;
+            margin: 16px 0 0 0;
+            font-size: 1.2rem;
+            height: auto;
+            line-height: initial;
+          }
+          .time {
+            float: right;
+            font-size: 1rem;
+            line-height: 1.6rem;
+            margin: 16px 0 0 0;
           }
         }
-        &:hover div{
-          opacity: 0.6;
+        hr {
+          margin: 4px 0;
+        }
+        .belong {
+          margin: 0 0 16px 0;
+          font-size: 1rem;
+          font-weight: bold;
+          color: $lightBlue;
+        }
+        .content {
+          .item-name {
+            font-weight: bold;
+          }
+          .item-content {
+            padding-left: 2rem;
+          }
         }
       }
       .work-list-leave-to, .work-list-enter {
